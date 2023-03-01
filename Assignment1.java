676239433109d3c0a21f6a2c275913c29a665657
Gitdurgabhavni <durgabhavani33312@gmail.com>
2023-03-01 16:23:51 +0530

    firstcommit

---
+++ Assignment1.java
@@ -0,0 +1,25 @@
+1.What is a computer?
+Ans. Computer is a electronic device for storing and processing data typically in binary form according to the instructions 
+given to it.
+
+2.What is a RAM?
+Ans. RAM is voltile which means when the power is off all the data which is present that will be erased.
+
+3. Where is data stored in a computer?
+Ans. Data stored in the hard disk.
+
+4.what is that input device used to type text amd numbers on a document in computer?
+Ans. Keboard
+
+5.What are the output devices?
+Ans. Monitor, Printer, Speaker ,
+
+6. what is the input devices allows a user to move the cursor on screen?
+Ans. Mouse
+
+7. Which language is directly understood by computer without a translation program?
+Ans.  Machine language
+
+8.What are input devices?
+Ans. Keyboard, mouse, scanner
+ 
